fac49a3ddeec3124394ee0040853beb00bf74ef9
KacBuj17 <kbujak150@interia.pl>
2026-01-23 12:13:38 +0100

    added script for prepare and upload embeddings to vector db

---
+++ test_and_scripts/scripts/custom_data_vector_db_run_tools/prepare_and_upload_embeddings.py
@@ -0,0 +1,64 @@
+import json
+import os
+import sys
+import time
+
+import fsspec
+import yaml
+
+from clip_retrieval import clip_inference
+
+
+def main():
+    resources_folder = os.environ.get("RESOURCES_FOLDER", "resources")
+    images_folder = os.path.join(resources_folder, os.environ.get("IMAGES_FOLDER", "images_folder"))
+    output_folder = os.path.join(resources_folder, os.environ.get("OUTPUT_FOLDER", "output_folder"))
+
+    with open("../configs/db_config.yaml", "r") as f:
+        db_config = yaml.safe_load(f)
+
+    fs, output_folder_in_fs = fsspec.core.url_to_fs(output_folder)
+    print(output_folder_in_fs)
+    if not fs.exists(output_folder_in_fs):
+        fs.mkdir(output_folder_in_fs)
+    embeddings_folder = os.path.join(output_folder, "embeddings")
+
+    inference_start_time = time.time()
+    clip_inference(
+        input_dataset=images_folder,
+        output_folder=embeddings_folder,
+        input_format="files",
+        enable_metadata=True,
+        enable_text=True,
+        write_batch_size=100000,
+        batch_size=512,
+        cache_path=None,
+        clip_model="ViT-B/32",
+        mclip_model="sentence-transformers/clip-ViT-B-32-multilingual-v1",
+        use_mclip=True,
+        writer_type=db_config["writer_type"],
+        mongo_uri=db_config["mongo_uri"],
+        mongo_db_name=db_config["mongo_db_name"],
+        qdrant_url=db_config["qdrant_url"],
+        qdrant_api=db_config["qdrant_api"],
+        collection_name=db_config["collection_name"],
+    )
+    inference_stop_time = time.time()
+    inference_duration = inference_stop_time - inference_start_time
+
+    stats = {
+        "inference_duration": inference_duration
+    }
+
+    output_file = os.path.join(resources_folder, "stats.json")
+
+    with open(output_file, "w", encoding="utf-8") as f:
+        json.dump(stats, f, indent=4)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e:
+        print(f"[FATAL] Unhandled exception: {e}", file=sys.stderr)
+        sys.exit(1)
